b1f4ea94d3318f0268843f57f8ea6ecec0ca27e2
mnmonherdene <mnmonherdene1234@gmail.com>
2022-11-21 13:14:01 +0800

    file upload done

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  signature() {
+    const timestamp = Math.round(new Date().getTime() / 1000);
+    const signature = cloudinary.utils.api_sign_request(
+      { timestamp },
+      process.env.CLOUDINARY_API_SECRET,
+    );
+
+    return { timestamp, signature };
   }
 }
