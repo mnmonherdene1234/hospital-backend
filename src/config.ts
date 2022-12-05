49bd47688498ce60276c85d42c0fa441eb365f54
mnmonherdene <mnmonherdene1234@gmail.com>
2022-12-05 11:14:20 +0800

    production

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const DATABASE_NAME = 'hospital',
   MONGODB_URI =
-    'mongodb+srv://mnmonherdene:MnMonherdene0529@cluster0.hlkavs8.mongodb.net/?retryWrites=true&w=majority',
+    'mongodb+srv://skintrainer:SkinTrainer123@cluster.bwao7at.mongodb.net/?retryWrites=true&w=majority',
   JWT_SECRET = '8-FxMmh2mJP4XJA9Gecu6q-vJrRJSPHfTurSy5xHDuI',
-  CLOUDINARY_CLOUD_NAME = 'dnxalrd2a',
-  CLOUDINARY_API_KEY = '682723139835675',
-  CLOUDINARY_API_SECRET = 'l-L6Zit2iAvMWHWFCN0JC0ZOTX8';
+  CLOUDINARY_CLOUD_NAME = 'dm0epppva',
+  CLOUDINARY_API_KEY = '475629784916411',
+  CLOUDINARY_API_SECRET = 'KtiCQ2Zt5xJep-levIT5DDsSCTU';
