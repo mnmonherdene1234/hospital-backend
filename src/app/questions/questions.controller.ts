29c282dcb7065203f26133b1afcaf14f3c9a56c4
mnmonherdene <mnmonherdene1234@gmail.com>
2022-12-28 10:34:31 +0800

    questions

--- src/app/questions/questions.controller.ts
+++ src/app/questions/questions.controller.ts
@@ -16,34 +16,38 @@ import { RolesGuard } from '../auth/roles.guard';
 import { QuestionDto } from './dto/question.dto';
 import { QuestionsService } from './questions.service';
 
-@Roles(Role.Admin)
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('questions')
 export class QuestionsController {
   constructor(private readonly questionsService: QuestionsService) {}
 
+  @Roles(Role.Admin)
   @Post()
   create(@Body() dto: QuestionDto, @Req() req: any) {
     dto.created_by = req.user.id;
     return this.questionsService.create(dto);
   }
 
+  @Roles(Role.Admin, Role.Worker)
   @Get()
   findAll() {
     return this.questionsService.findAll();
   }
 
+  @Roles(Role.Admin, Role.Worker)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.questionsService.findOne(id);
   }
 
+  @Roles(Role.Admin)
   @Put(':id')
   update(@Param('id') id: string, @Body() dto: QuestionDto, @Req() req: any) {
     dto.updated_by = req.user.id;
     return this.questionsService.update(id, dto);
   }
 
+  @Roles(Role.Admin)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.questionsService.remove(id);
