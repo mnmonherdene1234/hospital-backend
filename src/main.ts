6a4507387afb3a99118bff3a61be317f3c74d507
mnmonherdene1234 <mnmonherdene@gmail.com>
2023-01-08 06:14:36 +0000

    error fixed

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
       transformOptions: {
         enableImplicitConversion: true,
       },
+      forbidUnknownValues: false,
     }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
